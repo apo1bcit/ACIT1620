89b907b82a44abea9347b8379f1ae50ff5a03c01
apo1bcit <apo1@my.bcit.ca>
2025-03-19 09:25:39 -0700

    lab5

---
+++ week10/index.js
@@ -0,0 +1,12 @@
+function bark(dogname, dogweight) {
+    if (dogweight > 20) {
+        console.log(dogname + " says WOOF WOOF");
+    } else {
+        console.log(dogname + " says woof woof");
+    }
+}
+
+bark("Bobo:, 23");
+bark("Spiky", 17);
+bark("Spot", 53);
+bark("Lady", 16);
---
+++ week9/index4.js
@@ -0,0 +1,3 @@
+console.log(Math.floor(10 + Math.random() * 90)); // max: 99
+
+// Math.floor(min + Math.random() * (max - min + 1));
